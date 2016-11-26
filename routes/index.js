b86a0e053d25889ed1075137409a2ee134b2e3d8
Munir Barakat <barakat@hpe.com>
2016-11-26 21:58:11 +0200

    basic search is working now

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+(function() {
+  var express = require('express');
+  var router = express.Router();
+  var elasticSearch = require('elasticsearch');
+  var serverOptions =
+  {
+    hosts: [
+      'http://localhost:9200'
+    ]
+  };
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendfile('views/index.html');
-});
+  var elasticSearchClient = new elasticSearch.Client(serverOptions);
+  /* GET home page. */
 
-module.exports = router;
+  router.get('/', function(req, res) {
+    res.sendfile('views/index.html');
+  });
+
+  /* GET search results. */
+  router.post("/search", function(req, res) {
+    var termToSearch = req.body.query.termToSearch;
+    console.log("termToSearch=" + termToSearch);
+    elasticSearchClient.search({
+      index: 'people',
+      type: 'yellow_pages',
+      body: {
+        query: {
+          term: {name: termToSearch}
+        }
+      }
+    }, function(err, response) {
+      if (err) {
+        res.status(500);
+        console.log('error')
+      } else {
+        res.status(200).json(response);
+      }
+    });
+  });
+
+  module.exports = router;
+})();
--- views/searchController.js
+++ views/searchController.js
@@ -1,18 +1,36 @@
 (function(){
-  angular.module('yellowPages').controller('searchController', ['$scope', 'elasticClient', function($scope, elasticClient){
-    var PAGE_SIZE = 100;
-    elasticClient.search({
-      index: 'people',
-      q: '1315-69482',
-      size: PAGE_SIZE,
-      from: 0
-    }, function(error, response){
-      if(error){
-        console.log('ERROR:_____________________________________');
+  angular.module('yellowPages')
+  .controller('searchController', ['$scope', 'elasticClient', '$http', function($scope, elasticClient, $http){
+    $scope.results = [];
+    $scope.$watch('searchCriteria', function(data){
+      if(!data) return;
+      $http.post('/search', {
+        query: {
+          termToSearch: data
+        }
+      }).then(function success(res){
+        $scope.results = parseResponse(res);
+      }, function failure(error){
+        console.log('FAILURE');
         console.log(error);
-      } else {
-        console.log(response);
-      }
+      });
     });
+
+    function parseResponse(res) {
+      var results = [];
+      if(res && res.data && res.data.hits && res.data.hits.hits){
+        _.forEach(res.data.hits.hits, function(hit){
+          var item = {};
+          item.name = hit._source.name;
+          item.address = hit._source.address;
+          item.age = hit._source.birthday;
+          item.phone = hit._source.phone;
+          item.avatar = hit._source.avatar_origin;
+          results.push(item);
+        });
+      }
+      return results;
+    }
+
   }]);
 })();
